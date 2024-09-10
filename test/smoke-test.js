3c003e050c27160309436cbe4f614c453e00e79b
Zarahi93 <adahi.g.z@hotmail.com>
2024-09-10 14:43:24 -0600

    Minor changes

--- test/smoke-test.js
+++ test/smoke-test.js
@@ -4,69 +4,22 @@
 
 var assert = require('assert');
 
-// require the HelloWorld factory module
+// require the source module
 
-const helloFactory = require('../src');
+const sum = require('../src');
 
 describe('smoke test', () => {
-  it('should pass if mocha is working', done => {
-    done();
-  });
-  context('createHelloWorld', () => {
-    // factory method
-    it('should return an object that is not null', done => {
-      var hello = helloFactory.createHelloWorld();
-      assert.ok(hello, 'factory result should not be null');
-      done();
-    });
-  });
-  context('HelloWorld', () => {
-    context('greeting property', () => {
-      it('should have a greeting property', done => {
-        var hello = helloFactory.createHelloWorld();
-        assert.ok(hello.greeting,
-          'HelloWorld object should have a greeting property');
-        done();
-      });
-      it('should return default greeting if not set', done => {
-        var hello = helloFactory.createHelloWorld();
-        assert.equal(hello.greeting, 'Hello World!',
-          'HelloWorld greeting should be default if not set');
-        done();
-      });
-      it('should return set value', done => {
-        const TEST_GREETING = 'Yo World!'
-        var hello = helloFactory.createHelloWorld();
-        hello.greeting = TEST_GREETING;
-        assert.equal(hello.greeting, TEST_GREETING,
-          'HelloWorld greeting should return constructor greeting');
-        done();
-      });
-      it('should return constructor greeting if set', done => {
-        const TEST_GREETING = 'Yo World!'
-        const options = { greeting: TEST_GREETING };
-        var hello = helloFactory.createHelloWorld(options);
-        assert.equal(hello.greeting, TEST_GREETING,
-          'HelloWorld greeting should return constructor greeting');
-        done();
-      });
-    });
-    context('greet method', () => {
-      it('should have a greet method', done => {
-        var hello = helloFactory.createHelloWorld();
-        assert.ok(hello.greet,
-          'HelloWorld object should have a greet method');
-        done();
-      });
-      it('should be able to call greet method', done => {
-        var hello = helloFactory.createHelloWorld();
-        hello.greet();
-        done();
-      });
-      it('should be able to call greet method with new greeting', done => {
-        var hello = helloFactory.createHelloWorld();
-        hello.greeting = 'Yo World!';
-        hello.greet();
+  context('sum function', () => {
+    [
+      { values: [], expected: 0 },
+      { values: [20, 15], expected: 35 },
+      { values: [-2], expected: -2 },
+      { values: [20, 35, 40], expected: 95 },
+    ].forEach(function (el) {
+      it(`should sum ${(el.values.length > 0 ? el.values : 'an empty array' )} and return ${el.expected}`, done => {
+        const total = sum(el.values)
+        assert.equal(total, el.expected,
+          'not the expected return value');
         done();
       });
     });
